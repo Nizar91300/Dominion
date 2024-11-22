5bcdbe502d15bb8e710ae8a0225327f5eecd97c3
nizar91300 <nizarabdoul91300@gmail.com>
2024-11-22 21:12:58 +0100

    fix bot

--- include/Bot.h
+++ include/Bot.h
@@ -12,11 +12,7 @@ class Carte;
 class Bot : public Joueur {
 
 public:
-    Bot(Modele* modele) : Joueur(modele){
-        m_deckManager->addCardToMain(new Village(modele));
-        m_deckManager->addCardToMain(new Festin(modele));
-
-    }
+    Bot(Modele* modele) : Joueur(modele){}
 
     void playActionPhase(); // Gere la phase d'action
     void playBuyPhase(); // Gere la phase d'achat
